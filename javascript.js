7af125e80b35ff6336733932a1e15a6c57e172fa
MJerich <MatthewJerich@gmail.com>
2022-01-12 11:09:40 -0700

    alert fixed

--- javascript.js
+++ javascript.js
@@ -19,12 +19,12 @@ let buttons =  document.querySelectorAll('button');
 // use forEach method to iterate through the buttons
 buttons.forEach((button) => {
 
-    // listener for a click on witch button
-    button.addEventListener('click', () => {
+       // listener for a click on witch button
+     button.addEventListener('click', () => {
 
-        
+            
 
-        // Function for randomizing computers pick
+         // Function for randomizing computers pick
         function computerPlay() {
             const choices = ['Rock', 'Paper', 'Scissors'];
             const cpuChoice = Math.floor(Math.random()*choices.length);
@@ -56,12 +56,14 @@ buttons.forEach((button) => {
         if (userPick == cpuPick){
 
             console.log("tie");
+            document.getElementById("resultDisplay").innerHTML = 'You Tied';
         }   else if (
             (userPick == 'Rock') && (cpuPick == 'Paper') ||
             (userPick =='Paper') && (cpuPick == 'Scissors') ||
             (userPick == 'Scissors') && (cpuPick == 'Rock')
         ) {
             console.log('You Lose')
+            document.getElementById("resultDisplay").innerHTML = 'You Lose';
             cpuScore++;
         }   else if (
             (userPick == 'Rock') && (cpuPick == 'Scissors') ||
@@ -69,6 +71,7 @@ buttons.forEach((button) => {
             (userPick == 'Scissors') && (cpuPick == 'Paper')
         ) {
             console.log('You Win!')
+            document.getElementById("resultDisplay").innerHTML = 'You Win!';
             playerScore++;
         }   else {
             console.log('Invalid Input');
@@ -79,67 +82,15 @@ buttons.forEach((button) => {
         document.getElementById("playerScore").innerHTML = playerScore;
         document.getElementById("cpuScore").innerHTML = cpuScore;
 
-        // Alert the player if someone got 5 wins (win or lose) then refresh the page
-        if (playerScore == 5){
-            alert('YOU WIN! CLICK "OK" TO PLAY AGAIN.');
-            window.location.reload();
-        } else if (cpuScore == 5){
-            alert('Sorry, you lose. Click "ok" to play again.');
-            window.location.reload();
-        }
+        setTimeout(() => {
+            if (playerScore == 5){
+                alert('YOU WIN! CLICK "OK" TO PLAY AGAIN.');
+                window.location.reload();
+            } else if (cpuScore == 5){
+                alert('Sorry, you lose. Click "ok" to play again.');
+                window.location.reload();
+            }
+        }, 20);
 
+        });
     });
-});
-
-/*
-// Function to play the game.
-function game() {
-    while (playerScore<5 && cpuScore<5) {
-
-        // Get user pick
-        //let userPick = prompt('Rock, Paper, or Scissors?').toLowerCase();
-
-        // Function for randomizing computers pick
-        function computerPlay() {
-            const choices = ['Rock', 'Paper', 'Scissors'];
-            const cpuChoice = Math.floor(Math.random()*choices.length);
-            return choices[cpuChoice]
-        }
-
-        // Call funtion for the computers pick
-        let cpuPick = computerPlay()
-
-        // logging the two choices to make sure its right
-        console.log(userPick)
-        console.log(cpuPick)
-
-        // Output if the user won, lost, or tied
-        if (userPick == cpuPick.toLowerCase()) {
-            console.log('Tie');
-        }   else if (
-            (userPick == 'rock') && (cpuPick == 'Paper') ||
-            (userPick =='paper') && (cpuPick == 'Scissors') ||
-            (userPick == 'scissors') && (cpuPick == 'Rock')
-        ) {
-            console.log('You Lose')
-            cpuScore++;
-        }   else if (
-            (userPick == 'rock') && (cpuPick == 'Scissors') ||
-            (userPick == 'paper') && (cpuPick == 'Rock') ||
-            (userPick == 'scissors') && (cpuPick == 'Paper')
-        ) {
-            console.log('You Win!')
-            playerScore++;
-        }   else {
-            console.log('Invalid Input')
-        }
-        //show score at the end of each round
-        console.log('Player wins: ' + playerScore)
-        console.log('Computer wins: ' + cpuScore)
-    }
-    
-}
-
-// call the funtion to play the game
-game();
-*/
